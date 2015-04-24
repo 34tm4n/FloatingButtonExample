2bf65dea9927ca922293d6c5df76c909bae0a0f3
34tm4n <cerebalassassin17@gmail.com>
2015-04-23 21:38:34 -0700

    Initial commit

---
+++ app/src/main/java/com/example/wahib/floatingbuttonexample/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.wahib.floatingbuttonexample;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.melnykov.fab.FloatingActionButton;
+
+
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+
+    FloatingActionButton fab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(this);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==R.id.fab){
+            Toast.makeText(this,"Pressed",Toast.LENGTH_SHORT);
+        }
+    }
+}
